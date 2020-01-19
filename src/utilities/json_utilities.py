297ba71bd99d05012aaf05cb35a3ece464d3f0bb
Mason Rubik <mrubik@ualberta.ca>
2020-01-18 22:16:15 -0700

    Added sample config file

--- src/utilities/json_utilities.py
+++ src/utilities/json_utilities.py
@@ -21,9 +21,25 @@ def save_as_json(filename, data):
 
 if __name__ == "__main__":
     import os
-    import random
-    data = {str(k): random.random() for k in range(100)}
-    save_as_json("./database/test", data)
-    assert load_from_json("./database/test") == data
-    os.remove("./database/test.json")
+    # import random
+    # data = {str(k): random.random() for k in range(100)}
+    # save_as_json("./database/test", data)
+    # assert load_from_json("./database/test") == data
+    # os.remove("./database/test.json")
+    data = {}
+    data['Temperature_Low'] = 10
+    data['Temperature_Medium_Low'] = 21
+    data['Temperature_Median_High'] = 35
+    data['Temperature__High'] = 37
+    data['Temperature_Target'] = 29
+    data['Sunlight_Low'] = 6
+    data['Sunlight_High'] = 9
+    data['Sunlight_Target'] = 8
+    data['Moisture_Low'] = 55
+    data['Moisture_High'] = 80
+    data['Moisture_Target'] = 65
+    save_as_json("./configuration_files/sample.cfg", data)
+    # assert load_from_json("./database/test") == data
+    # os.remove("./database/test.json")
+
 
