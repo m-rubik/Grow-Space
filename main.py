44dda4ad4b30bb6a120ca82500dad2fe72e9eccf
Mason Rubik <mrubik@ualberta.ca>
2020-01-28 16:23:44 -0700

    Test of small LED strip

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ class ThreadedClient:
             self.controls['fan'] = sim_relay.Relay(pin=17, name="fan")
             self.controls['pump'] = sim_relay.Relay(pin=22, name="pump")
             self.controls['UV LED'] = sim_relay.Relay(pin=27, name="UV LED")
-            self.controls['RGB LED'] = sim_led_strip.LEDStrip(LED_PIN=18, name="RGB LED")
+            self.controls['RGB LED'] = sim_led_strip.LEDStrip(LED_PIN=18, LED_COUNT=9, name="RGB LED")
         else:
             from src.controls import fan, pump, uv_led, led_strip
             self.controls['fan'] = fan.Fan(pin=17, name="fan", queue=Queue())
