82e2ab0f968e93d3c1214f4f81ce1fc98c04ee43
Sigte <49625118+sigma-dante@users.noreply.github.com>
2020-03-11 00:06:02 -0600

    PEP8 Changes

--- src/sensors/soil_moisture_sensor.py
+++ src/sensors/soil_moisture_sensor.py
@@ -63,10 +63,10 @@ class SoilMoistureSensor(Sensor):
         self._current_val[2]= (-100/(self.max_volt-self.min_volt))* (self._current_val[1]-self.max_volt)
         if self._current_val[2] > 99.7:
             self._current_val[2] = 100.00
-        elif self._current_val[2]< 0.3:
+        elif self._current_val[2] < 0.3:
             self._current_val[2] = 0.00
             
-        self.queue.put(round(self._current_val[2],2)) # This is passing the raw value to the queue?
+        self.queue.put(round(self._current_val[2], 2))
 
         # Step 3: Log the reading
         current_time = datetime.now()
@@ -75,6 +75,7 @@ class SoilMoistureSensor(Sensor):
 
     def shutdown(self):
         print(self.name, "shutting down.")
+
 
 if __name__ == "__main__":
 
